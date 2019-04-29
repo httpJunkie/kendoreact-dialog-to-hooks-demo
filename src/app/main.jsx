35f332c75067c4973dcec8f2419013351980e47b
Eric Bishard <ebishard@progress.com>
2019-04-29 01:42:16 -0700

    functional hook syntax

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -1,36 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import { Dialog, DialogActionsBar } from '@progress/kendo-react-dialogs';
 import { Button } from '@progress/kendo-react-buttons';
 
-class DialogWrapper extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { visible: true };
-    this.toggleDialog = this.toggleDialog.bind(this);
-  }
+const DialogWrapper = () => {;
+  const [visible, setVisible] = useState(true);
+  const toggleVisible = () => setVisible(!visible);
 
-  toggleDialog() {
-    this.setState({
-      visible: !this.state.visible
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <Button className="k-button" onClick={this.toggleDialog}>Open Dialog</Button>
-        {this.state.visible && <Dialog title={"Please confirm"} onClose={this.toggleDialog}>
-          <p style={{ margin: "25px", textAlign: "center" }}>Are you sure you want to continue?</p>
-          <DialogActionsBar>
-            <Button className="k-button" onClick={this.toggleDialog}>No</Button>
-            <Button className="k-button" onClick={this.toggleDialog}>Yes</Button>
-          </DialogActionsBar>
-        </Dialog>}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Button className="k-button" onClick={toggleVisible}>Open Dialog</Button>
+      {visible && <Dialog title={"Please confirm"} onClose={toggleVisible}>
+        <p style={{ margin: "25px", textAlign: "center" }}>Are you sure you want to continue?</p>
+        <DialogActionsBar>
+          <Button className="k-button" onClick={toggleVisible}>No</Button>
+          <Button className="k-button" onClick={toggleVisible}>Yes</Button>
+        </DialogActionsBar>
+      </Dialog>}
+    </div>
+  );
 }
 
 ReactDOM.render(<DialogWrapper />,document.querySelector('my-app'));
-
